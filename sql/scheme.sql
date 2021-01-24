987e40c74fe736e7a827e47d4ee107986f20d7bd
salvoronis <salvoronis@gmail.com>
2021-01-25 00:15:25 +0300

    sql fix

--- sql/scheme.sql
+++ sql/scheme.sql
@@ -1,90 +1,87 @@
-create table class (
-	class_id	serial primary key,
-	name		varchar(3) unique not null
+create table member (
+	userId serial primary key,
+	login varchar(30) unique not null,
+	password varchar(64) not null,
+	firstname varchar(25) not null,
+	secondname varchar(25) not null,
+	email varchar(50) not null
 );
 
-create table student (
-	student_id	serial primary key,
-	class		integer references class(class_id) on delete cascade on update cascade,
-	nickname	varchar(30) unique not null,
-	password	varchar(64) not null,
-	name		varchar(35) not null
+create table roles(
+	roleId serial primary key,
+	name varchar(20) unique not null
 );
 
-create table headman (
-	student_id	integer references student(student_id) on delete cascade on update cascade,
-	class_id	integer references class(class_id) on delete cascade on update cascade
-);
-
-create table teacher (
-	teacher_id	serial primary key,
-	name		varchar(35) not null,
-	nickname	varchar(30) unique not null,
-	password	varchar(64) not null
-);
-
-create table class_teacher (
-	class_id	integer references class(class_id) on delete cascade on update cascade,
-	teacher_id	integer references teacher(teacher_id) on delete cascade on update cascade
-);
-
-create table roles (
-	role_id		serial primary key,
-	name		varchar(20) unique not null
-);
-
-create table teacher_roles (
-	roles_id	integer references roles(role_id) on delete cascade on update cascade,
-	teacher_id	integer references teacher(teacher_id) on delete cascade on update cascade
+create table user_roles (
+	role_id integer references roles(roleId) on delete cascade on update cascade,
+	user_id integer references member(userId) on delete cascade on update cascade
 );
 
 create table news (
-	news_id		serial primary key,
-	header		varchar(50) not null,
-	tags		text,
-	body		text not null,
-	date		date not null
+	newsId serial primary key,
+	header varchar(50) not null,
+	body text not null,
+	date date not null
 );
 
 create table news_author (
-	teacher_id	integer references teacher(teacher_id) on delete cascade on update cascade,
-	news_id		integer references news(news_id) on delete cascade on update cascade
+	news_id integer references news(newsId) on delete cascade on update cascade,
+	author_id integer references member(userId) on delete cascade on update cascade
+);
+
+create table class (
+	classId serial primary key,
+	name varchar(5) unique not null
+);
+
+create table class_user (
+	student_id integer unique references member(userId) on delete cascade on update cascade,
+	class_id integer references class(classId) on delete cascade on update cascade
+);
+
+create table headman (
+	student_id integer references member(userId) on delete cascade on update cascade,
+	class_id integer references class(classId) on delete cascade on update cascade
+);
+
+create table class_teacher (
+	class_id integer references class(classId) on delete cascade on update cascade,
+	teacher_id integer references member(userId) on delete cascade on update cascade
 );
 
 create table subject (
-	subject_id	serial primary key,
-	name		varchar(50) unique not null,
-	course_hours	integer,
-	credits		integer
+	subjectId serial primary key,
+	name varchar(25) unique not null,
+	course_hours integer
+);
+
+create table subject_class (
+	class_id integer references class(classId) on delete cascade on update cascade,
+	subject_id integer references subject(subjectId) on delete cascade on update cascade
 );
 
 create table teacher_subject (
-	teacher_id	integer references teacher(teacher_id) on delete cascade on update cascade,
-	subject_id	integer references subject(subject_id) on delete cascade on update cascade
+	teacher_id integer references member(userId) on delete cascade on update cascade,
+	subject_id integer references subject(subjectId) on delete cascade on update cascade
+);
+
+create table student_mark (
+	markId serial primary key,
+	student_id integer references member(userId) on delete cascade on update cascade,
+	teacher_id integer references member(userId) on delete cascade on update cascade check (student_id != teacher_id),
+	mark integer not null,
+	subject_id integer references subject(subjectId) on delete cascade on update cascade
 );
 
 create table schedule (
-	schedule_id	serial primary key,
-	dayoweek	varchar(3) not null,
-	time		varchar(5) not null,
-	room		integer,
-	class_id	integer references class(class_id) on delete cascade on update cascade
+	scheduleId serial primary key,
+	class_id integer references class(classId) on delete cascade on update cascade,
+	dayoweek varchar(10) not null,
+	time varchar(5) not null,
+	room integer not null
 );
 
-create table schedule_of_subject (
-	schedule_id	integer references schedule(schedule_id) on delete cascade on update cascade,
-	subject_id	integer references subject(subject_id) on delete cascade on update cascade
-);
-
-create table subject_of_class (
-	class_id	integer references class(class_id) on delete cascade on update cascade,
-	subject_id	integer references subject(subject_id) on delete cascade on update cascade
-);
-
-create table students_mark (
-	mark_id		serial primary key,
-	value		integer,
-	student		integer references student(student_id) on delete cascade on update cascade,
-	teacher		integer references teacher(teacher_id) on delete cascade on update cascade,
-	subject		integer references subject(subject_id) on delete cascade on update cascade
+create table subject_schedule(
+	subject_id integer references subject(subjectId) on delete cascade on update cascade,
+	schedule_id integer references schedule(scheduleId) on delete cascade on update cascade
 );
